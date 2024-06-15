482b929d40da3bd5aae5a0b72bae2145bf9e7cd0
Atonnesen <tonnesen@umich.edu>
2024-06-14 22:56:22 -0400

    First Attempt

--- Lab7.py
+++ Lab7.py
@@ -1,6 +1,6 @@
 # CSC 122
 # Substitution Cipher
-# By <Your Name>
+# By <Ashley Tonnesen>
 
 """
 Inputs, Processes, Outputs (IOP)
@@ -9,9 +9,29 @@ Input(s):
 Processes:
 Output(s):
 """
+# prompt user to enter short message to be encrypted with sub cypher
+# python functions ordinal value = ord
+# python functions character = chr
+# prompt user to enter encrypted message to be decrypted
 
 def main():
-    pass # replace this line with all of your solution code
+    # cipher is your empty list
+    cipher = []
+    message = input("Enter the message to be encrypted: ")
+    for letter in message:
+        # convert it to ordinal value and add 3 then apend to cypher list
+        cipher.append(chr(ord(letter) + 3))
+    # combine everything into one list
+    print(f"Encrypted message = {''.join(cipher)}\n")
+
+    # plaintext is empty list
+    plaintext = []
+    message = input("Enter the cipher to be decrypted: ")
+    for letter in message:
+        # convert it to ordinal value and subtract 3 then apend to plaintext list
+        plaintext.append(chr(ord(letter) - 3))
+    # combine everything into one list
+    print(f"Decrypted message = {''.join(plaintext)}\n")
 
 
 if __name__ == '__main__':
